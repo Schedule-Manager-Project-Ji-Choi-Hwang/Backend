a226fb5279bbe28f12a9ac80f71f916d6287f6b4
hji1235 <hji1231@daum.net>
2023-11-30 05:58:32 +0900

    RequestDataValidation Edit

--- src/main/java/backend/schedule/validation/RequestDataValidation.java
+++ src/main/java/backend/schedule/validation/RequestDataValidation.java
@@ -1,16 +1,9 @@
 package backend.schedule.validation;
 
-import backend.schedule.dto.MessageReturnDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
-@Component
 public class RequestDataValidation {
 
     public static void beanValidation(BindingResult bindingResult) {
