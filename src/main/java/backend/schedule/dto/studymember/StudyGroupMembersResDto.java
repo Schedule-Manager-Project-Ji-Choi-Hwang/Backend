5fc7c50d197ed64192f2e765b90b4218a0cbef1f
FFG811 <jiminkyu0463@gmail.com>
2023-12-14 17:37:10 +0900

    DTO Edit

--- src/main/java/backend/schedule/dto/studymember/StudyGroupMembersResDto.java
+++ src/main/java/backend/schedule/dto/studymember/StudyGroupMembersResDto.java
@@ -5,6 +5,8 @@ import backend.schedule.enumlist.ConfirmAuthor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import static backend.schedule.enumlist.ConfirmAuthor.*;
+
 @Getter
 @NoArgsConstructor
 public class StudyGroupMembersResDto {
@@ -15,9 +17,14 @@ public class StudyGroupMembersResDto {
 
     private ConfirmAuthor authority;
 
+    private boolean leader;
+
     public StudyGroupMembersResDto(StudyMember studyMember) {
         this.studyMemberId = studyMember.getId();
         this.nickname = studyMember.getMember().getNickname();
         this.authority = studyMember.getConfirmAuthor();
+
+        if (studyMember.getConfirmAuthor() == LEADER) leader = true;
+        else leader = false;
     }
 }
